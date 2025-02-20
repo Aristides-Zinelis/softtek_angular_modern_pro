61cb89cdb26fe420640a0b4e454bc89cc53335b2
Alberto Basalo <albertobasalo@hotmail.com>
2025-02-20 18:02:02 +0100

    Enhance logging functionality; include application name in LoggerService

--- assets-board/src/app/app.config.ts
+++ assets-board/src/app/app.config.ts
@@ -5,12 +5,13 @@ import {
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { withAppName } from "./logger/app-name.token";
 import { provideLogger } from "./logger/logger.provider";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideLogger(),
+    provideLogger(withAppName("Assets Board")),
   ],
 };
--- assets-board/src/app/logger/logger.service.ts
+++ assets-board/src/app/logger/logger.service.ts
@@ -4,6 +4,7 @@ import { APP_NAME } from "./app-name.token";
 @Injectable()
 export class LoggerService {
   private appName = inject(APP_NAME);
+
   public log(message: string) {
     console.log(`[${this.appName}]: ${message}`);
   }
