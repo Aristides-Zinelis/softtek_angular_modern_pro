c1410379e6070ecc1348222d81b9fd97ce655cdb
Alberto Basalo <albertobasalo@hotmail.com>
2025-02-25 17:03:09 +0100

    Refactor TotalAmountComponent to format currency display; use computed property for improved performance

--- assets-board/src/app/layout/header/total-amount.component.ts
+++ assets-board/src/app/layout/header/total-amount.component.ts
@@ -1,11 +1,20 @@
-import { Component, inject } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { AssetsStore } from "app/state/assets.store";
 
 @Component({
   selector: "lab-total-amount",
-  template: `<a>{{ totalAmount() }}</a>`,
+  template: `<a>{{ formatCurrency() }}</a>`,
 })
 export class TotalAmountComponent {
   private assetsStore = inject(AssetsStore);
   protected totalAmount = this.assetsStore.totalAmount;
+
+  protected formatCurrency = computed(() => {
+    const value = this.totalAmount();
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      maximumFractionDigits: 0,
+    });
+  });
 }
